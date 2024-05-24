76416674db4fdef7524ed230a028fd7b1f1eaf31
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-24 20:26:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 st.write("Hello, let's learn how to build a streamlit app together")
 
-df = pd.read_csv('data/cc_rfm.csv)
+df = pd.read_csv('data/cc_rfm.csv')
 st.dataframe(df)
 
 rand = np.random.normal(1, 2, size = 20)
