90379e9b5ce233238bcbb64c027fb331599781a3
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-25 01:00:23 +0800

    Update content_samples.py

--- content_samples.py
+++ content_samples.py
@@ -1,12 +1,13 @@
 # Cheat sheet
-
+import streamlit as st
 # Add markdown
 st.title ("this is the app title")
 st.header("this is the markdown")
 st.markdown("this is the header")
 st.subheader("this is the subheader")
 st.caption("this is the caption")
-st.code("x=2021")st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 
 # Display image
