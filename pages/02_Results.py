9e77e28c073d835b7191f3093ed9af65304ee806
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-24 22:52:10 +0800

    Update 02_Results.py

--- pages/02_Results.py
+++ pages/02_Results.py
@@ -4,3 +4,16 @@ st.title("Results")
 st.write("Here you can put each of your key results.")
 
 st.header("1. Top customer spending behavior by category")
+
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+   {
+       "col1": list(range(20)) * 3,
+       "col2": np.random.randn(60),
+       "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
+   }
+)
+
+st.bar_chart(chart_data, x="col1", y="col2", color="col3")
