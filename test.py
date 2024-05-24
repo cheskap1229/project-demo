4d357ef3900f50b4511cfc5a969c66627921d2cb
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-24 22:49:04 +0800

    Update test.py
    edit plot

--- test.py
+++ test.py
@@ -4,10 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 st.write("Hello, let's learn how to build a streamlit app together")
 
-df = pd.read_csv('data/cc_rfm.csv')
-st.dataframe(df)
+chart_data = pd.DataFrame(
+   {
+       "col1": list(range(20)) * 3,
+       "col2": np.random.randn(60),
+       "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
+   }
+)
 
-rand = np.random.normal(1, 2, size = 20)
-fig, ax = plt.subplots()
-ax.hist(rand, bins=15)
-st.pyplot(fig)
+st.bar_chart(chart_data, x="col1", y="col2", color="col3")
+# df = pd.read_csv('data/cc_rfm.csv')
+# st.dataframe(df)
+
+# rand = np.random.normal(1, 2, size = 20)
+# fig, ax = plt.subplots()
+# ax.hist(rand, bins=15)
+# st.pyplot(fig)
