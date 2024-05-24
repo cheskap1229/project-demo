76ebd67b64d71d0516879fcd0140741af185915f
cheskap1229 <148071219+cheskap1229@users.noreply.github.com>
2024-05-24 23:14:38 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 st.write("Hello, let's learn how to build a streamlit app together")
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
-
-
-# df = pd.read_csv('data/cc_rfm.csv')
-# st.dataframe(df)
-
-# rand = np.random.normal(1, 2, size = 20)
-# fig, ax = plt.subplots()
-# ax.hist(rand, bins=15)
-# st.pyplot(fig)
